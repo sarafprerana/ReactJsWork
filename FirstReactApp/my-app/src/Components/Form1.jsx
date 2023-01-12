e5d664722831d77d089b066be4886d53b12af902
myapp <sarafprerana1@gmail.com>
2023-01-12 18:02:05 +0530

    update form

--- FirstReactApp/my-app/src/Components/Form1.jsx
+++ FirstReactApp/my-app/src/Components/Form1.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react'
-import { Box, Button, Checkbox, Container, CssBaseline, Divider, FormControl, FormControlLabel, FormGroup,  FormLabel, InputLabel, MenuItem,
-     Radio, RadioGroup, Select, TextField, Typography } from '@mui/material';
+import { Box, Button, Checkbox, Container, CssBaseline, Divider, FormControl, FormControlLabel, FormGroup,  FormHelperText,  FormLabel, Grid, InputLabel, MenuItem,
+     Radio, RadioGroup, Select, TextField} from '@mui/material';
 import { useState } from "react";
 import Confirmation from './Confirmation';
-
 
 const Form1=(props)=> {
     const[firstname, setFirstName]=useState("" );
@@ -13,12 +12,12 @@ const Form1=(props)=> {
     const [age, setAge]=useState(" ");
     const[course,setCourse]=useState(" ");
     // const[checked,setChecked]=useState(" ");
-    const [coffee, setCheckBox1] = useState();
-    const[coffee2, setCheckBox2]=useState();
-    const[coffee3, setCheckBox3]=useState();
-    const[cofee4, setCheckBox4]=useState();
-    //  const[label,setLabel]=useState("");
-
+    const [coffee,setCoffee] = useState("")
+    const [espresso,setEspresso]= useState("")
+    const [cappachinoo,setCappachinoo]= useState("")
+    const [flatwhite,setFlatwhite]= useState("")
+    const [longblack,setLongblack]= useState("")
+   
      const [completeForm, setCompleteForm]=useState( );
      const[formToPropagate, setFormToPropagate]=useState( );
 
@@ -65,43 +64,51 @@ const handleDevpChange=(event)=>{
 
  const handleSubmit=()=>{
    console.log(completeForm);
-   setFormToPropagate(completeForm);
+  setFormToPropagate(completeForm);
   };
 
-  const onCheckBox1Change = (event) => {
-    setCheckBox1(event.target.value);
-    const form = { ...completeForm };
-    form.coffee1 = event.target.value;
+ const oncoffeechange = (event)=>{
+ console.log('Cofee i Like >>', event.target.value);
+ setCoffee(event.target.value);
+ const form={...completeForm};
+ form.coffee=event.target.value;
+ setCompleteForm(form);
+  };
+ 
+ const onCheckBox1Change = (event)=>{
+//  console.log('confee i like >>', event.target.value);                       
+ setEspresso(event.target.value);
+ const form={...completeForm};
+ form.coffee=event.target.value;
+ setCompleteForm(form);
 
+                        };
+
+  const onCheckBox2Change = (event) => {
+    setCappachinoo(event.target.value);
+    const form = { ...completeForm };
+    form.coffee = event.target.value;
     setCompleteForm(form);
 }
-const onCheckBox2Change = (event) => {
-    setCheckBox2(event.target.value);
-    const form = { ...completeForm };
-    form.coffee2 = event.target.value;
-
-    setCompleteForm(form);
-}
-
 const onCheckBox3Change = (event) => {
-    setCheckBox3(event.target.value);
+    setFlatwhite(event.target.value);
     const form = { ...completeForm };
-    form.coffee3 = event.target.value;
-
+    form.coffee = event.target.value;
     setCompleteForm(form);
 }
+
 const onCheckBox4Change = (event) => {
-    setCheckBox4(event.target.value);
+    setLongblack(event.target.value);
     const form = { ...completeForm };
-    form.coffee4 = event.target.value;
-
+    form.coffee = event.target.value;
     setCompleteForm(form);
 }
+
 
  useEffect(() => {
     setGender('female');
-    setAge(20);
-}, [setGender]);
+    setAge('Age');
+}, [setGender][setAge]);
 
 return (
     <>
@@ -109,75 +116,96 @@ return (
           <CssBaseline />
           <Container maxWidth="md">
              {/* <Typography variant='h3'>Form</Typography> */}
-             <Box sx={{display:"flex", flexDirection:'column' ,padding:5,border:' 2px solid black'}} >
+             <Box sx={{display:"flex", flexDirection:'column' , bgcolor: '#cfe8fc',padding:5,border:' 2px solid black'}} >
              <FormControl >
                   <Box sx={{height:"10px"}}/>
                   <Box sx={{bgcolor:"orange",display:'flex',flexDirection:'row',justifyContent:'center'}}>Addmission Form</Box><br />
                   <Divider />
                  
-                  <FormLabel id=" " >Personal Info</FormLabel><br />
-                   <Box sx={{dispaly:'flex',border:"1px dashed teal" ,padding:2 , justifyContent:"space-between"}}>
+                  <FormLabel sx={{color:'blue'}}>Personal Info</FormLabel><br />
+                   {/* <Grid container spacing={4}>
+                    <Grid item xs={4}> */}
+                   <Box sx={{dispaly:'flex',border:"1px dashed teal" ,padding:2 , justifyContent:"space-between"}}> 
                   <TextField  id="demo-helper-text-aligned" label="FirstName" value={firstname} variant="outlined"  onChange={onFirstNameChange}/>
+                  {/* </Grid>
+                  <Grid item xs={4}> */}
                   <TextField id="middlename" label="MiddleName"  value={middlename} variant="outlined"  onChange={onChangeMiddleName}/>
-                  <TextField id="lastname" label="LastName" value={lastname} variant="outlined" onChange={onChangeLastName} /> 
-                  </Box><br />
+                  {/* </Grid>
+                  <Grid item xs={4}> */}
+                  <TextField id="lastname" label="LastName" value={lastname} variant="outlined" onChange={onChangeLastName} />
+                   </Box> 
+                  {/* </Grid>
+                  </Grid> */}
+                  </FormControl>
+                
                   
-                <FormLabel id=" ">Gender</FormLabel>
+                <FormLabel sx={{color:'blue'}}>Gender</FormLabel>
                 <Box sx={{display:'flex'}}>
+                {/* <Grid container spacing={4}>
+                    <Grid item xs={6}> */}
                 <RadioGroup aria-labelledby="demo-radio-buttons-group-label" Value={gender} name="radio-buttons-group" onChange={onGenderFieldChange}>
                       <FormControlLabel value="female" control={<Radio />} label="Female" />
                       <FormControlLabel value="male" control={<Radio />} label="Male" />
                       <FormControlLabel value="other" control={<Radio />} label="Other" />
-                </RadioGroup><br />
-                <Box sx={{ display: 'flex' }}>
-                    <FormControl sx={{ width:300,marginLeft:50 }} component="fieldset" variant="standard">
-                    <FormLabel component="legend">Coffee I Like</FormLabel>
+                </RadioGroup>
+                {/* </Grid>
+                </Grid> */}
+                
+                {/* <Grid container spacing={4}>
+                    <Grid item xs={6}> */}
+                    <FormControl sx={{width:300,marginLeft:30}}>
+                    <FormLabel sx={{color:'blue'}} value={"coffee"} onChange>Coffee I Like</FormLabel>
+                    <Box sx={{ display: 'flex' }}>
                     <FormGroup>
-                        <FormControlLabel control={<Checkbox value="espresso " onClick={onCheckBox1Change} name="espresso" /> } label="espresso" />
-                        <FormControlLabel control={<Checkbox value="cappachinoo "onClick={onCheckBox2Change}name="cappachinoo" />}label="cappachinoo"/>
-                        <FormControlLabel control={<Checkbox value="flat white "onClick={onCheckBox3Change}name="flatwhite" />}label="flat white"/>
-                        <FormControlLabel control={<Checkbox value="long blank " onClick={onCheckBox4Change} name="longbank" /> } label="long blank"/>
+                        <FormControlLabel control={<Checkbox  value={'espresso'} onChange={onCheckBox1Change} name="espresso" /> } label="espresso" />
+                        <FormControlLabel control={<Checkbox value={'cappachinoo'} onClick={onCheckBox2Change}name="cappachinoo" />}label="cappachinoo"/>
+                        <FormControlLabel control={<Checkbox value={'flatwhite'} onClick={onCheckBox3Change}name="flatwhite" />}label="flat white"/>
+                        <FormControlLabel control={<Checkbox value={'longblack'}  onClick={onCheckBox4Change} name="longblack" /> } label="long black"/>
                     </FormGroup>
+                    </Box>
                     </FormControl>
+                    {/* </Grid>
+                    </Grid> */}
+                
                 </Box>
-                </Box><br />
-                  {/* <InputLabel id="  ">Age</InputLabel> */}
-                  <FormControl sx={{width:300}}>
-                  <InputLabel id="  ">Age</InputLabel>
-                  <Select
-                   labelId="Age " id="Age" label="Age" value={age} onChange={handleAgeChange}>
-                    
+                  <br/>
+                  {/* <Grid container spacing={4}>
+                    <Grid item xs={4}> */}
+                    <Box>
+                   <FormControl sx={{width:300}}>
+                  <InputLabel sx={{colr:'blue'}}>Age</InputLabel>
+                  <Select labelId="Age " id="Age" label="Age" value={age} onChange={handleAgeChange}>
                        <MenuItem value={10}>Ten</MenuItem>
                        <MenuItem value={20}>Twenty</MenuItem>
                        <MenuItem value={30}>Thirty</MenuItem>
-                     
                   </Select>
-                    <FormControl sx={{width:300,marginLeft:60}}>
+                  <FormHelperText>10,20,30,etc...</FormHelperText>
+                  </FormControl> 
+                    {/* </Grid>
+                     <Grid item xs={4}> */}
+                     <FormControl sx={{width:300,marginLeft:10}}>
                     <InputLabel id="  ">DeveloperIn</InputLabel>
                   <Select labelId="course" id="course" label="course" value={course} onChange={handleDevpChange}>
-                    
-                       <MenuItem value={course}>UI</MenuItem>
-                       <MenuItem value={course}>DataBase</MenuItem>
-                       <MenuItem value={course}>DevOps</MenuItem>
-                       <MenuItem value={course}>API</MenuItem>
-                     
+                       <MenuItem value={"UI"}>UI</MenuItem>
+                       <MenuItem value={"DataBase"}>DataBase</MenuItem>
+                       <MenuItem value={"DevOps"}>DevOps</MenuItem>
+                       <MenuItem value={"API"}>API</MenuItem>
                   </Select>
-                  </FormControl>
-
-                  </FormControl>
-              </FormControl>
-  
-  
-             {/* <pre>FirstName:{firstname}<br/>
+                  <FormHelperText>UI,API,DataBase,DevOps</FormHelperText>
+                  </FormControl>  
+                  {/* </Grid>
+                    </Grid> */}
+                 
+                  </Box>
+              {/* <pre>FirstName:{firstname}<br/>
                   MiddleName:{middlename}<br />
                   LastName:{lastname} <br />
                   Gender: {gender}<br />
                   Age:{age}
-             </pre> */}
+             </pre>  */}
              {/* <pre>{JSON.stringify(completeForm,null,3)}</pre> */}
              <Box sx={{marginY: 2,display: "flex",justifyContent: "flex-end",}}>
              <Button variant="contained" onClick={handleSubmit} >SUBMIT</Button> 
-
              </Box>
           </Box>
           </Container><br/>
